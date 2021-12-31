9252e3b62639653df23fe42aa87f0e4acbcebd38
Diego Rodrigues <diegokod@gmail.com>
2021-12-31 20:21:26 -0300

    agendaV1

---
+++ contatos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria', 'mostrar')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_editable = ('telefone', 'mostrar')
+
+
+
+admin.site.register(Categoria)
+
+admin.site.register(Contato, ContatoAdmin)
---
+++ contatos/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.core.paginator import Paginator
+from .models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+
+
+def index(request):
+    contatos = Contato.objects.order_by('-data_criacao').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 5)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/index.html', {
+       'contatos': contatos
+
+    })
+
+def ver_contato(request, contato_id):
+    # contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
+    return render(request, 'contatos/ver_contato.html', {
+    'contato': contato
+
+    })
+    # except Contato.DoesNotExist as e:
+    #     raise Http404()
+
+def busca(request):
+    termo = request.GET.get('termo')
+    campos = Concat('nome', Value(''), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        nome_completo__icontains=termo,
+    )
+
+    # contatos = Contato.objects.order_by('-data_criacao').filter(
+    #     Q(nome__icontains=termo) | Q(sobrenome__icontains=termo),
+    #     mostrar=True
+    # )
+
+
+    paginator = Paginator(contatos, 5)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
+
+    })
+
