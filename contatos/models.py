f36781ae3e702b1674f83738ea4bdeb4acb5c702
Diego Rodrigues <diegokod@gmail.com>
2022-01-02 17:58:12 -0300

    v2_agenda

--- contatos/models.py
+++ contatos/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome
 
+
 class Contato(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=150)
     sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
@@ -16,6 +18,8 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/')
 
     def __str__(self):
         return self.nome
+
